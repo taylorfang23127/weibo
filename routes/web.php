c90cf8901eaca4c8d49640286e36f8a60f8dc671
Taylor <1663395071@qq.com>
2020-01-12 15:22:05 +0000

    创建会话

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 Route::get('signup','UsersController@create')->name('signup');
 Route::resource('users','UsersController');
+
+Route::get('login','SessionController@create')->name('login');
+Route::post('login','SessionController@store')->name('login');
+Route::delete('loginout','SessionController@destroy')->name('logout');
+
