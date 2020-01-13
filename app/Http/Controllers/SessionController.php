2c3d0e23f1210a456572bbc9e507e43e47873a50
Taylor <1663395071@qq.com>
2020-01-13 05:23:12 +0000

    用户退出登录

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -28,4 +28,10 @@ class SessionController extends Controller
             return redirect()->back()->withInput();
         }
     }
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success','您已成功退出!');
+        return redirect('login');
+    }
 }
